e925323478ade8b5ea95573c5d131d237f96349f
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-02-04 20:15:19 +0100

    fix find-and-set-font

--- elisp-utils.el
+++ elisp-utils.el
@@ -16,7 +16,7 @@
 
 (defun find-and-set-font (candidates)
   "Find valid font in CANDIDATES and set it as the default font face."
-  (when-let ((font (find-if (lambda (f) (find-font (font-spec :name f))) candidates)))
+  (when-let (font (find-if (lambda (f) (find-font (font-spec :name f))) candidates))
     (set-face-attribute 'default nil :font font)))
 
 (provide 'elisp-utils)
