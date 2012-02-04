cf3b2762d94c498af117add9eeae80c03960b436
Aristid Breitkreuz <aristidb@googlemail.com>
2012-02-04 19:44:40 +0100

    fix #1 - add documentation

--- elisp-utils.el
+++ elisp-utils.el
@@ -1,14 +1,24 @@
-;; Just a few simple emacs lisp utility functions.
+;;; elisp-utils.el --- Just a few simple emacs lisp utility functions
+
+
+;;; Commentary:
+;; This is intended to be included early in your .emacs, and used for utility purposes.
 
 (require 'cl)
 
+;;; Code:
+
 (defun add-exec-paths (paths)
+  "Add list of paths (PATHS) to $PATH environment variable and Emacs' `exec-path' variable (to the front)."
   (let ((expanded (mapcar 'expand-file-name paths)))
     (setenv "PATH" (concat (mapconcat 'identity expanded ":") ":" (getenv "PATH")))
     (setq exec-path (append expanded exec-path))))
 
 (defun find-and-set-font (candidates)
+  "Find valid font in CANDIDATES and set it as the default font face."
   (let ((font (find-if (lambda (f) (find-font (font-spec :name f))) candidates)))
     (when font (set-face-attribute 'default nil :font font))))
 
 (provide 'elisp-utils)
+
+;;; elisp-utils.el ends here
