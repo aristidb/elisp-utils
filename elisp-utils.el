63bc2f49eb6a6a02be0735b0cf7aa33d00786d81
Aristid Breitkreuz <aristidb@googlemail.com>
2012-02-04 10:58:14 +0100

    add find-and-set-font

--- elisp-utils.el
+++ elisp-utils.el
@@ -4,3 +4,8 @@
   (let ((expanded (mapcar 'expand-file-name paths)))
     (setenv "PATH" (concat (mapconcat 'identity expanded ":") ":" (getenv "PATH")))
     (setq exec-path (append expanded exec-path))))
+
+(defun find-and-set-font (candidates)
+  (let ((font (find-if (lambda (f) (find-font (font-spec :name f))) candidates)))
+    (set-face-attribute 'default nil :font font))
+)
